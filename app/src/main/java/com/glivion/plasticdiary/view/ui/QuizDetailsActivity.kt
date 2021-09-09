43be6c45adc8dca74e6cfdbaa3c03bf5fa154633
Emmanuel Owusu <e.owusu@tinydavid.com>
2021-09-09 16:29:52 +0000

    Quiz page completed

---
+++ app/src/main/java/com/glivion/plasticdiary/view/ui/QuizDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.glivion.plasticdiary.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.glivion.plasticdiary.R
+
+class QuizDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_details)
+    }
+}
